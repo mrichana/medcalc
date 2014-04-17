5e8d319a7334624b651d132a9c8562eadb1d75dc
mrichana <mrichana@gmail.com>
2014-04-17 09:48:54 +0300

    MedicalFile Started

--- js/app.js
+++ js/app.js
@@ -15,4 +15,19 @@
     function ($locationProvider) {
       $locationProvider.html5Mode(false);
     });
+
+  angular.module('medicalFile', [
+      'medicalFile.controllers',
+      'medicalFile.filters',
+      'medicalFile.services',
+      'medicalFile.directives',
+      'ngTouch',
+      'ngAnimate',
+      'ngSanitize',
+      'ngStorage',
+      'ui.bootstrap'
+    ],
+    function ($locationProvider) {
+      $locationProvider.html5Mode(false);
+    });
 })();
